6d18cf7b479a1a9ff44cd71d2b739189f3f559b2
VMware {code} <noreply@vmware.com>
2023-08-23 16:56:14 +0000

    Initial commit by Eduardo Meirelles

--- NotifySlack-77632eefbf9d2260aecfb9d2f437c597d80d8b3c99093be59d3dbf02.py
+++ NotifySlack-77632eefbf9d2260aecfb9d2f437c597d80d8b3c99093be59d3dbf02.py
@@ -10,12 +10,17 @@ def handler(context, inputs):
     abx_deploy = inputs["deploymentName"]
     abx_desc = inputs["description"]
     abx_action = inputs["eventType"]
+    abx_status = inputs["status"]
+    abx_msg = inputs["failureMessage"]
     # Set messagen based if the deploymnet was a creation or destruction
     if abx_action == "CREATE_DEPLOYMENT" : 
+      if abx_status == "FAILED" :
+        mytext = "WARNING: the deployment " + abx_deploy + " failed: " + abx_msg
+      else :
         mytext = "A new resources has just been provisioned sucessfully called: " + abx_deploy + " and the requester is: " + abx_owner + " with the justification of: " + abx_desc
     elif abx_action == "DESTROY_DEPLOYMENT" :
-        mytext = "The deployment called: " + abx_deploy + " has just been deleted by: " + abx_owner
+      mytext = "The deployment called: " + abx_deploy + " has just been deleted by: " + abx_owner
     else :
-        mytext = "Ops something went wrong with deployment: " + abx_deploy + " because of: " + abx_action + " better to check it out"
+      mytext = "Ops something went wrong with deployment: " + abx_deploy + " because of: " + abx_action + " better to check it out"
     slack_data = {'text': mytext}
     response = requests.post(webhook_url, data = json.dumps(slack_data), headers = {'Content-Type': 'application/json'})
